534372a5df8fdce838af81ad5b2db258a3e96806
charttrakarn0784 <charttrakarn6@hotmail.co.th>
2022-09-15 14:47:02 +0700

    finished

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -2,12 +2,45 @@ import React from "react";
 import Reply from "./Reply";
 
 export default function Comment(props) {
+  // console.log(props.replies);
+  console.log(props.replies);
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
-
+      <div className="d-flex gap-2 my-2">
+        <img
+          src={props.userImagePath}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#E5E7EB" }}
+        >
+          <span className="fw-semibold">{props.username}</span>
+          <br />
+          <span>{props.commentText}</span>
+          {props.likeNum > 0 && (
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span className="text-muted">{props.likeNum} คน</span>
+            </div>
+          )}
+        </div>
+      </div>
       {/*render Reply here... */}
+      {/* 
+      {props.replies.map((reply, i) => (
+        <Reply
+          key={i}
+          username={reply[i].username}
+          userImagePath={reply[i].userImagePath}
+          replyText={reply[i].replyText}
+          likeNum={reply[i].likeNum}
+        />
+      ))} */}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Comment from "../components/Comment";
 import PostOwner from "../components/PostOwner";
+import Reply from "../components/Reply";
 import { comments } from "../libs/comments";
 
 export default function Home() {
@@ -13,7 +14,7 @@ export default function Home() {
         style={{ maxWidth: "700px" }}
         className="mx-auto bg-white p-3 rounded rounded-3 shadow-sm"
       >
-        {/* PostOwner Example*/}
+        {/* PostOwner Example
         <div className="vstack gap-3">
           <div className="d-flex align-items-center gap-3">
             <img
@@ -31,10 +32,39 @@ export default function Home() {
             <span className="text-muted">100 คน</span>
           </div>
           <hr className="m-0" />
-        </div>
-
+        </div> */}
+        <PostOwner
+          username="Charttrakarn Choosiri 620610784"
+          userImagePath="/profileImages/pepe.jpg"
+          commentText="Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207"
+          likeNum={100}
+        />
+        {comments.map((comment, i) => (
+          <Comment
+            key={i}
+            username={comments[i].username}
+            userImagePath={comments[i].userImagePath}
+            commentText={comments[i].commentText}
+            likeNum={comments[i].likeNum}
+            replies={comments[i].replies}
+          />
+        ))}
+        {/* <Comment
+          username={comments[0].username}
+          userImagePath={comments[0].userImagePath}
+          commentText={comments[0].commentText}
+          likeNum={comments[0].likeNum}
+          replies={comments[0].replies}
+        />
+        <Comment
+          username={comments[1].username}
+          userImagePath={comments[1].userImagePath}
+          commentText={comments[1].commentText}
+          likeNum={comments[1].likeNum}
+          replies={comments[1].replies}
+        /> */}
         {/* Comment Example */}
-        <div className="d-flex gap-2 my-2">
+        {/* <div className="d-flex gap-2 my-2">
           <img
             src="/profileImages/lisa.jpg"
             width="48"
@@ -54,9 +84,9 @@ export default function Home() {
               <span className="text-muted">999 คน</span>
             </div>
           </div>
-        </div>
+        </div> */}
 
-        {/* Reply Example */}
+        {/* Reply Example
         <div className="d-flex gap-2 my-2 ps-5">
           <img
             src="/profileImages/puppy.jpg"
@@ -73,7 +103,7 @@ export default function Home() {
             <br />
             <span>จริงค้าบบบบบบบบ</span>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
